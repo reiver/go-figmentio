fcd28dd746843e8fca294d3000416e7c325006e3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-02 12:35:18 -0700

    polygon

---
+++ polygon/addresses.go
@@ -0,0 +1,10 @@
+package polygon
+
+// FigmentValidatorAddresses is figment.io's validator node address.
+//
+// This value was taken from:
+// https://figment.io/staking/protocols/polygon/
+//
+// THIS IS AN IMPORTANT VALUE.
+// FOR YOUR OWN SANITY, YOU SHOULD DOUBLE CHECK THAT THIS IS ACCURATE.
+const FigmentValidatorAddresses string = "0x1efecb61a2f80aa34d3b9218b564a64d05946290"
