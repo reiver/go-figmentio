7f00321688b77e759b690e3feb25af35829870d2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-02 12:53:19 -0700

    sei

---
+++ sei/addresses.go
@@ -0,0 +1,10 @@
+package sei
+
+// FigmentValidatorAddresses is figment.io's validator node address.
+//
+// This value was taken from:
+// https://figment.io/staking/protocols/sei/
+//
+// THIS IS AN IMPORTANT VALUE.
+// FOR YOUR OWN SANITY, YOU SHOULD DOUBLE CHECK THAT THIS IS ACCURATE.
+const FigmentValidatorAddresses string = "seivaloper1y82m5y3wevjneamzg0pmx87dzanyxzht0kepvn"
