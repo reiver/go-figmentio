24f71a752e114ab58e03c3d76ab879d8d115f557
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-02 12:51:00 -0700

    thegraph

---
+++ thegraph/addresses.go
@@ -0,0 +1,10 @@
+package thegraph
+
+// FigmentValidatorAddresses is figment.io's validator node address.
+//
+// This value was taken from:
+// https://figment.io/staking/protocols/the-graph/
+//
+// THIS IS AN IMPORTANT VALUE.
+// FOR YOUR OWN SANITY, YOU SHOULD DOUBLE CHECK THAT THIS IS ACCURATE.
+const FigmentValidatorAddresses string = "0x6ac85b9d834b51b14a7b0ed849bb5199e04c05c5"
