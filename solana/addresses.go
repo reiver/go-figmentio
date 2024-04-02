a5a54a1ccc11b9d00e2bf6be600a7271b7c1a028
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-02 12:39:30 -0700

    solana

---
+++ solana/addresses.go
@@ -0,0 +1,10 @@
+package solana
+
+// FigmentValidatorAddresses is figment.io's validator node address.
+//
+// This value was taken from:
+// https://figment.io/staking/protocols/
+//
+// THIS IS AN IMPORTANT VALUE.
+// FOR YOUR OWN SANITY, YOU SHOULD DOUBLE CHECK THAT THIS IS ACCURATE.
+const FigmentValidatorAddresses string = "CcaHc2L43ZWjwCHART3oZoJvHLAe9hzT2DJNUpBzoTN1"
