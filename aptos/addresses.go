b0f4b5c5bf0cd598e292a471f236f17535416c1f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-02 12:32:01 -0700

    aptos

---
+++ aptos/addresses.go
@@ -0,0 +1,10 @@
+package aptos
+
+// FigmentValidatorAddresses is figment.io's validator node address.
+//
+// This value was taken from:
+// https://figment.io/staking/protocols/aptos/
+//
+// THIS IS AN IMPORTANT VALUE.
+// FOR YOUR OWN SANITY, YOU SHOULD DOUBLE CHECK THAT THIS IS ACCURATE.
+const FigmentValidatorAddresses string = "346361873c235e1a300fd67a2e8b7bd1ea3744001b2e0d1942317c4c8521f5dd"
