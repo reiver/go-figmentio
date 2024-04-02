310f2bb7d33d088cabb8f57af99f45c825c10d88
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-02 12:49:12 -0700

    zetachain

---
+++ zetachain/addresses.go
@@ -0,0 +1,10 @@
+package zetachain
+
+// FigmentValidatorAddresses is figment.io's validator node address.
+//
+// This value was taken from:
+// https://figment.io/staking/protocols/zetachain/
+//
+// THIS IS AN IMPORTANT VALUE.
+// FOR YOUR OWN SANITY, YOU SHOULD DOUBLE CHECK THAT THIS IS ACCURATE.
+const FigmentValidatorAddresses string = "zetavaloper1hjct6q7npsspsg3dgvzk3sdf89spmlpf6rc87c"
