adb22c4875c2876553650ee49debde45f6559311
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-02 12:38:05 -0700

    axelar

---
+++ axelar/addresses.go
@@ -0,0 +1,10 @@
+package axelar
+
+// FigmentValidatorAddresses is figment.io's validator node address.
+//
+// This value was taken from:
+// https://figment.io/staking/protocols/axelar/
+//
+// THIS IS AN IMPORTANT VALUE.
+// FOR YOUR OWN SANITY, YOU SHOULD DOUBLE CHECK THAT THIS IS ACCURATE.
+const FigmentValidatorAddresses string = "axelarvaloper1uvx854yjzn9re8vu74067u68r4ar70tywgpcwg"
